13c83285f31e4a0beabca3566c37d9130b928bed
Mike Knoop <mikeknoop@gmail.com>
2025-10-19 19:26:13 -0700

    Fix HA compat issue entry setup for remote platform

--- custom_components/samsungmdc-remote/__init__.py
+++ custom_components/samsungmdc-remote/__init__.py
@@ -16,12 +16,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "remote")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
